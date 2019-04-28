414a21823c11486a599b0b6643b8b425d614ca8d
David Kim <d.kim1423@gmail.com>
2019-04-28 16:25:10 +1200

    heyyyyyyyyyy broooooooooooo

--- index.js
+++ index.js
@@ -1,80 +1,132 @@
-var flat;
-var flong;
-var fname;
-var fmessage;
-var rootRef = firebase.database().ref().child("Users1");
-rootRef.on("child_added", snap =>{
-  var name = snap.child("Name").val();
-  var message = snap.child("Message").val();
-  var longii = snap.child("long").val();
-  var latii = snap.child("lat").val();
-  flat = latii;
-  flong = longii;
-  fname = name;
-  fmessage = message;
-})
+var mapMarkers = {}
 
-var flat2;
-var flong2;
-var fname2;
-var fmessage2;
-var rootRef2 = firebase.database().ref().child("Users2");
-rootRef2.on("child_added", snap =>{
-  var name2 = snap.child("Name").val();
-  var message2 = snap.child("Message").val();
-  var longii2 = snap.child("long").val();
-  var latii2 = snap.child("lat").val();
-  flat2 = latii2;
-  flong2 = longii2;
-  fname2 = name2;
-  fmessage2 = message2;
-})
+function getMarker(keyName) {
+  return mapMarkers[keyName]
+}
 
-var flat3;
-var flong3;
-var fname3;
-var fmessage3;
-var rootRef3 = firebase.database().ref().child("Users3");
-rootRef3.on("child_added", snap =>{
-  var name3 = snap.child("Name").val();
-  var message3 = snap.child("Message").val();
-  var longii3 = snap.child("long").val();
-  var latii3 = snap.child("lat").val();
-  flat3 = latii3;
-  flong3 = longii3;
-  fname3 = name3;
-  fmessage3 = message3;
-})
+function retrieveMarker(databaseRef, keyName) {
+  var ref = databaseRef.child(keyName)
+
+  ref.on("child_added", snap => {
+    var name = snap.child("name").val();
+    var message = snap.child("message").val();
+    var long = snap.child("long").val();
+    var lat = snap.child("lat").val();
+    // mapMarkers[keyName] = {
+    mapMarkers[snap.key] = {
+      lat,
+      long,
+      message,
+      name
+    }
+  })
+
+  ref.on("child_changed", snap => {
+    var name = snap.child("name").val();
+    var message = snap.child("message").val();
+    var long = snap.child("long").val();
+    var lat = snap.child("lat").val();
+    // mapMarkers[keyName] = {
+    mapMarkers[snap.key] = {
+      lat,
+      long,
+      message,
+      name
+    }
+  })
+}
+
+retrieveMarker(firebase.database().ref(), "Users")
+
+function createMarker(databaseRef, keyName, nameRef, messageRef) {
+  var ref = databaseRef.child(keyName).push()
+  navigator.geolocation.getCurrentPosition(function(pos){
+    ref.set({
+      name: nameRef.value,
+      message: messageRef.value,
+      long: pos.coords.longitude,
+      lat: pos.coords.latitude
+    })
+    alert("Submission successful, check your position on Google Maps to see updates")
+  },showError);
+}
+
+function updateMarker(databaseRef, keyName, nameRef, messageRef) {
+  var ref = databaseRef.child(keyName)
+  var nameText = nameRef.value;
+  ref.child("user").child("Name").set(nameText);
+  var messageTxt = messageRef.value;
+  ref.child("user").child("Message").set(messageTxt);
+  navigator.geolocation.getCurrentPosition(function(pos){
+    ref.child("user").child("long").set(pos.coords.longitude);
+    ref.child("user").child("lat").set(pos.coords.latitude);
+  }, showError);
+}
+
 function messageClick1(){
-  var rootRef = firebase.database().ref().child("Users1");
-  var nameText = nameField1.value;
-  rootRef.child("user").child("Name").set(nameText);
-  var messageTxt = messageField1.value;
-  rootRef.child("user").child("Message").set(messageTxt);
-  navigator.geolocation.getCurrentPosition(function(pos){
-    rootRef.child("user").child("long").set(pos.coords.longitude);
-    rootRef.child("user").child("lat").set(pos.coords.latitude);
+  createMarker(firebase.database().ref(), "Users", nameField1, messageField1)
+}
+
+function showPosition() {
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showMap, showError);
+  } else{
+    alert("Sorry, your browser does not support HTML5 geolocation.");
+  }
+}
+
+function putMarkerOnMap(markerInfo, map) {
+  var latilongi = new google.maps.LatLng(markerInfo.lat, markerInfo.long);
+  var marker = new google.maps.Marker({position:latilongi,map:map, title: markerInfo.name});
+  var infowindow = new google.maps.InfoWindow({
+    content: markerInfo.name + ": "+ markerInfo.message
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
   });
 }
-function messageClick2(){
-  var rootRef2 = firebase.database().ref().child("Users2");
-  var nameText = nameField2.value;
-  rootRef2.child("user").child("Name").set(nameText);
-  var messageTxt = messageField2.value;
-  rootRef2.child("user").child("Message").set(messageTxt);
-  navigator.geolocation.getCurrentPosition(function(pos){
-    rootRef2.child("user").child("long").set(pos.coords.longitude);
-    rootRef2.child("user").child("lat").set(pos.coords.latitude);
+
+function putMarkersOnMap(markerInfo, map) {
+  var latilongi = new google.maps.LatLng(markerInfo.lat, markerInfo.long);
+  var marker = new google.maps.Marker({position:latilongi,map:map, title: markerInfo.name});
+  var infowindow = new google.maps.InfoWindow({
+    content: markerInfo.name + ": "+ markerInfo.message
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
   });
 }
-function messageClick3(){
-  var rootRef3 = firebase.database().ref().child("Users3");
-  var nameText = nameField3.value;
-  rootRef3.child("user").child("Name").set(nameText);
-  var messageTxt = messageField3.value;
-  rootRef3.child("user").child("Message").set(messageTxt);
-  navigator.geolocation.getCurrentPosition(function(pos){
-    rootRef3.child("user").child("long").set(pos.coords.longitude);
-    rootRef3.child("user").child("lat").set(pos.coords.latitude);
-  });
+
+// Define callback function for successful attempt
+function showMap(position){
+  // Get location data
+  lat = position.coords.latitude;
+  long = position.coords.longitude;
+  var latlong = new google.maps.LatLng(lat, long);
+
+  var options = {
+    center: latlong,
+    zoom: 16,
+    mapTypeControl: true,
+    navigationControlOptions: {style:google.maps.NavigationControlStyle.SMALL}
+  }
+
+  var map = new google.maps.Map(document.getElementById("embedMap"), options);
+
+  for(var markerInfo in mapMarkers) {
+    putMarkerOnMap(getMarker(markerInfo), map)
+  }
+}
+
+// Define callback function for failed attempt
+function showError(error){
+  if(error.code == 1){
+    alert("You've decided not to share your position, but it's OK. We won't ask you again.")
+  } else if(error.code == 2){
+    alert("The network is down or the positioning service can't be reached.")
+  } else if(error.code == 3){
+    alert("The attempt timed out before it could get the location data.")
+  } else{
+    alert("Geolocation failed due to unknown error.")
+  }
 }
